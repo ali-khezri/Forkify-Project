a15e17cb8b0bdc730551d9ed98c2a3cb9c93dfb0
Ali Khezri <khezri.ali.1994@gmail.com>
2025-10-06 18:23:07 +0330

    Initial commit

---
+++ src/js/helpers.js
@@ -0,0 +1,35 @@
+import { TIMEOUT_SEC } from './config.js';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(
+        new Error(`Request took too long! Timeout after ${TIMEOUT_SEC} second`)
+      );
+    }, s * 1000);
+  });
+};
+
+export const AJAX = async function (url, uploadData = undefined) {
+  try {
+    const fetchPro = uploadData
+      ? fetch(url, {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
+
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
